f782ca5390ebedd53826112c972524968edfd7a1
ArikAx <axmatov17@gmai.com>
2024-08-19 06:37:56 +0300

    new print

--- arik_axmatov.java
+++ arik_axmatov.java
@@ -2,13 +2,13 @@ public class arik_axmatov {
     public static void main(String[] args){
         
             System.out.println("Arik Axmatov" );
-            System.out.println("Arik Axmatov" );
-            System.out.println("Arik Axmatov" );
-            System.out.println("Arik Axmatov" );
-            System.out.println("Arik Axmatov" );
-            System.out.println("Arik Axmatov" );
-            System.out.println("Arik Axmatov" );
-            System.out.println("Arik Axmatov" );
+            System.out.println("Arik Axmato" );
+            System.out.println("Arik Axmat" );
+            System.out.println("Arik Axma" );
+            System.out.println("Arik Axm" );
+            System.out.println("Arik Ax" );
+            System.out.println("Arik A" );
+            System.out.println("Arik " );
 
     }
     
